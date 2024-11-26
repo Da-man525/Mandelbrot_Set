c32b02ec776797fdfebdc0f2dea46869898968fb
dman525 <dman525gaming@gmail.com>
2024-11-25 17:34:13 -0800

    added includes

--- ComplexPlane.h
+++ ComplexPlane.h
@@ -1,5 +1,14 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <iostream>
+#include <sstream>
+#include <vector>
+
 const unsigned int MAX_ITER = 64;
 const float BASE_WIDTH = 4.0;
 const float BASE_HEIGHT = 4.0;
 const float BASE_ZOOM = 0.5;
+
+
+
 

f6750708d514443814c1555f54d32623599c4a21
Da-man525 <102446876+Da-man525@users.noreply.github.com>
2024-11-25 18:55:24 -0800

    Update ComplexPlane.cpp
    Created Constructor

--- ComplexPlane.cpp
+++ ComplexPlane.cpp
@@ -1,1 +1,20 @@
+#include "ComplexPlane.cpp"
 
+using namespace sf;
+
+ComplexPlane::ComplexPlane(int pixelWidth, int pixelHeight){
+  
+  m_pixel_width = pixelWidth;
+  m_pixel_height = pixelHeight;
+  m_vArray = VertexArray(Points, m_pixel_width * m_pixel_height);
+
+  m_plane_center = sf::Vector2f(0.0f, 0.0f);
+  m_plane_size = sf::Vector2f(BASE_WIDTH, BASE_HEIGHT);
+
+  m_aspectRatio = static_cast<float>(m_pixel_width) / m_pixel_height;
+
+  m_mouseLocation = Vector2f(0.0f, 0.0f);
+
+  m_state = State::DISPLAYING;
+  m_zoomCount = 0;
+}

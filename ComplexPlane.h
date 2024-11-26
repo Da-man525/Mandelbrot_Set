f6c9f400417231c1d20c47e4e355b76e7e00c76f
RobeenAziz <robeen.aziz115@gmail.com>
2024-11-25 18:23:12 -0800

    Update ComplexPlane.h

--- ComplexPlane.h
+++ ComplexPlane.h
@@ -5,6 +5,7 @@
 #include <vector>
 #include <SFML/Graphics/Drawable.hpp>
 #include <SFML/Graphics/VertexArray.hpp>
+using namespace sf;
 
 const unsigned int MAX_ITER = 64;
 const float BASE_WIDTH = 4.0;
@@ -14,6 +15,20 @@ const float BASE_ZOOM = 0.5;
 enum class State {
     CALCULATING,
     DISPLAYING
+};
+
+class ComplexPlane
+{
+public:
+	ComplexPlane(int pixelWidth, int pixelHeight);
+	void draw(RenderTarget& target, RenderStates states) const;
+	void zoomIn();
+	void zoomOut();
+	void setCenter(Vector2i mousePixel);
+	void setMouseLocation(Vector2i mousePixel);
+	void loadText(Text& text);
+	void updateRender();
+
 };
 
 

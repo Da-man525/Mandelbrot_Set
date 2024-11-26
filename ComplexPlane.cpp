bfc8dfe40c3235a3e212e3c6df33ce32241c596c
Da-man525 <102446876+Da-man525@users.noreply.github.com>
2024-11-25 19:07:22 -0800

    Update ComplexPlane.cpp

--- ComplexPlane.cpp
+++ ComplexPlane.cpp
@@ -4,14 +4,15 @@ using namespace sf;
 
 ComplexPlane::ComplexPlane(int pixelWidth, int pixelHeight){
   
-m_pixel_size.x = pixelWidth;
-m_pixel_size.y = pixelHeight;
-m_vArray = VertexArray(Points, m_pixel_size.x * m_pixel_size.y);
+  m_pixel_size.x = pixelWidth;
+  m_pixel_size.y = pixelHeight;
+  m_vArray = VertexArray(Points, m_pixel_size.x * m_pixel_size.y);
 
-m_plane_center = sf::Vector2f(0.0f, 0.0f);
-m_plane_size = sf::Vector2f(BASE_WIDTH, BASE_HEIGHT);
+  m_plane_center = sf::Vector2f(0.0f, 0.0f);
 
-m_aspectRatio = static_cast<float>(m_pixel_size.x) / m_pixel_size.y;
+
+  m_aspectRatio = static_cast<float>(m_pixel_size.x) / m_pixel_size.y;
+  m_plane_size = sf::Vector2f(BASE_WIDTH, BASE_HEIGHT * m_aspectRatio);
 
   m_mouseLocation = Vector2f(0.0f, 0.0f);
 

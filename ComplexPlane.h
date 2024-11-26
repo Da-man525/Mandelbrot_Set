5ebd7da12e075281026922de2ed0d79d8b2c5d96
RobeenAziz <robeen.aziz115@gmail.com>
2024-11-25 18:44:25 -0800

    Update ComplexPlane.h

--- ComplexPlane.h
+++ ComplexPlane.h
@@ -5,7 +5,7 @@
 #include <vector>
 #include <SFML/Graphics/Drawable.hpp>
 #include <SFML/Graphics/VertexArray.hpp>
-using namespace sf;
+#include "ComplexPlane.cpp"
 
 const unsigned int MAX_ITER = 64;
 const float BASE_WIDTH = 4.0;

7838ce6970f92ccd1813f7ee0598713a869493d1
RobeenAziz <robeen.aziz115@gmail.com>
2024-11-26 20:15:44 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,46 @@
+#include "ComplexPlane.h"
+using namespace sf;
 
+int main()
+{
+	VideoMode vm(1920, 1080);
+
+	RenderWindow window(vm, "MandelBrot Set", Style::Default);
+
+	ComplexPlane game = ComplexPlane(1920, 1080);
+	Font font;
+	Text text;
+	Event event;
+	while (window.isOpen())
+	{
+		if (event.type == Event::Closed)
+		{
+			window.close();
+		}
+		if (event.type == Event::MouseButtonPressed)
+		{
+			if (event.mouseButton.button == Mouse::Right)
+			{
+				game.zoomOut();
+				game.setCenter(Mouse::getPosition());
+			}
+			else if (event.mouseButton.button == Mouse::Left)
+			{
+				game.zoomIn();
+				game.setCenter(Mouse::getPosition());
+			}
+		}
+		if (event.type == Event::MouseMoved)
+		{
+			game.setMouseLocation(Mouse::getPosition());
+		}
+		if (Keyboard::isKeyPressed(Keyboard::Escape))
+		{
+			window.close();
+		}
+		game.updateRender();
+		game.loadText(text);
+		window.clear();
+		game.draw(text, );
+	}
+}

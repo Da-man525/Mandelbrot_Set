53cad3323de7fbe414ceec706816b6c258a7762c
Da-man525 <102446876+Da-man525@users.noreply.github.com>
2024-11-26 20:56:31 -0800

    Update ComplexPlane.cpp

--- ComplexPlane.cpp
+++ ComplexPlane.cpp
@@ -3,22 +3,17 @@
 using namespace sf;
 using namespace std;
 
-ComplexPlane::ComplexPlane(int pixelWidth, int pixelHeight){
-  
-  m_pixel_size.x = pixelWidth;
-  m_pixel_size.y = pixelHeight;
-  m_vArray = VertexArray(Points, m_pixel_size.x * m_pixel_size.y);
-
-  m_plane_center = sf::Vector2f(0.0f, 0.0f);
-
-
-  m_aspectRatio = static_cast<float>(m_pixel_size.x) / m_pixel_size.y;
-  m_plane_size = sf::Vector2f(BASE_WIDTH, BASE_HEIGHT * m_aspectRatio);
-
-  m_mouseLocation = Vector2f(0.0f, 0.0f);
-
-  m_state = State::CALCULATING;
-  m_zoomCount = 0;
+ComplexPlane::ComplexPlane(int pixelWidth, int pixelHeight)
+{
+    m_pixel_size.x = pixelWidth;
+    m_pixel_size.y = pixelHeight;
+    m_plane_size.x = BASE_WIDTH;
+    m_plane_size.y = BASE_HEIGHT;
+    m_plane_center.x = 0.0f;
+    m_plane_center.y = 0.0f;
+    m_zoomCount = 0;
+    m_aspectRatio = static_cast<float>(pixelWidth) / static_cast<float>(pixelHeight);
+    m_vArray = VertexArray(Points, m_pixel_size.x * m_pixel_size.y);
 }
 
 void ComplexPlane::draw(RenderTarget& target, RenderStates states) const{

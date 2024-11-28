0254cedfd58889ccc58ef458de6e69c7489553c7
RobeenAziz <robeen.aziz115@gmail.com>
2024-11-27 17:05:52 -0800

    Update ComplexPlane.cpp

--- ComplexPlane.cpp
+++ ComplexPlane.cpp
@@ -5,20 +5,21 @@ using namespace std;
 
 ComplexPlane::ComplexPlane(int pixelWidth, int pixelHeight)
 {
-    m_pixel_size.x = pixelWidth;
-    m_pixel_size.y = pixelHeight;
-    m_plane_size.x = BASE_WIDTH;
-    m_plane_size.y = BASE_HEIGHT;
-    m_plane_center.x = 0.0f;
-    m_plane_center.y = 0.0f;
-    m_zoomCount = 0;
-    m_aspectRatio = static_cast<float>(pixelWidth) / static_cast<float>(pixelHeight);
-    m_vArray = VertexArray(Points, m_pixel_size.x * m_pixel_size.y);
-    m_state = State::CALCULATING;
+	m_pixel_size.x = pixelWidth;
+	m_pixel_size.y = pixelHeight;
+	m_plane_size.x = BASE_WIDTH;
+	m_plane_size.y = BASE_HEIGHT;
+	m_plane_center.x = 0.0f;
+	m_plane_center.y = 0.0f;
+	m_zoomCount = 0;
+	m_aspectRatio = static_cast<float>(pixelWidth) / static_cast<float>(pixelHeight);
+	m_vArray = VertexArray(Points, m_pixel_size.x * m_pixel_size.y);
+	m_state = State::CALCULATING;
 }
 
-void ComplexPlane::draw(RenderTarget& target, RenderStates states) const{
-  target.draw(m_vArray);
+
+void ComplexPlane::draw(RenderTarget& target, RenderStates states) const {
+	target.draw(m_vArray);
 }
 
 void ComplexPlane::zoomIn()
@@ -54,19 +55,19 @@ void ComplexPlane::setMouseLocation(Vector2i mousPixel)
 	m_mouseLocation = currcoord;
 }
 
-int ComplexPlane::countIterations(Vector2f coord){
+int ComplexPlane::countIterations(Vector2f coord) {
 	Vector2f z(0.0f, 0.0f);
 
 	const unsigned int maxIterations = MAX_ITER;
 
-	for(int i = 0; i < maxIterations; i++){
+	for (int i = 0; i < maxIterations; i++) {
 		float realPart = pow(z.x, 2) - pow(z.y, 2) + coord.x;
 		float imaginaryPart = 2.0f * z.x * z.y + coord.y;
 
 		z.x = realPart;
 		z.y = imaginaryPart;
 
-		if(pow(z.x, 2) * pow(z.y, 2) > 4.0f){
+		if (pow(z.x, 2) * pow(z.y, 2) > 4.0f) {
 			return i;
 		}
 	}
@@ -75,63 +76,61 @@ int ComplexPlane::countIterations(Vector2f coord){
 
 void ComplexPlane::loadText(Text& text)
 {
-    stringstream outputtext;
+	stringstream outputtext;
 	outputtext << "Mandelbrot Set" << endl << "Center: (" << m_plane_center.x << "," << m_plane_center.y << ")" << endl;
 	outputtext << "Cursor: (" << m_mouseLocation.x << "," << m_mouseLocation.y << ")" << endl;
 	outputtext << "Left-click to zoom in" << endl << "Right-click to zoom out";
 	string outtext = outputtext.str();
-	Font font;
-	font.loadFromFile("arial.ttf");
-	text.setFont(font);
 	text.setCharacterSize(24);
-	text.setFillColor(Color::Red);
+	text.setFillColor(Color::Green);
 	text.setString(outtext);
 
 }
 
 void ComplexPlane::iterationsToRGB(size_t count, Uint8& r, Uint8& g, Uint8& b) {
-
 	if (count == MAX_ITER) {
-	r = 0;
-	g = 0;
-	b = 0;
-        }
-       else if (count >= 0 && count <= 12) {
-	r = 191;
-	g = 0;
-	b = 255;
-        }
-	else if (count >= 13 && count <= 25) {
 		r = 0;
-		g = 251;
+		g = 0;
+		b = 0;
+	}
+	else if (count >= 0 && count <= 12) {
+		r = 255;
+		g = 255;
 		b = 255;
 	}
+	else if (count >= 13 && count <= 25) {
+		r = 105;
+		g = 10;
+		b = 91;
+	}
 	else if (count >= 26 && count <= 38) {
-		r = 0;
-		g = 255;
-		b = 72;
+		r = 233;
+		g = 38;
+		b = 84;
 	}
 	else if (count >= 39 && count <= 51) {
-		r = 238;
-		g = 255;
-		b = 0;
+		r = 237;
+		g = 145;
+		b = 1;
 	}
 	else {
-		r = 255;
-		g = 60;
-		b = 0;
+		r = 0;
+		g = 245;
+		b = 237;
 	}
 }
 
 Vector2f ComplexPlane::mapPixelToCoords(Vector2i mousePixel) {
-    float mappedX = (static_cast<float>(mousePixel.x) / m_pixel_size.x) * m_plane_size.x
-                    + (m_plane_center.x - m_plane_size.x / 2.0f);
+	float mappedX = (static_cast<float>(mousePixel.x) / m_pixel_size.x) * m_plane_size.x
+		+ (m_plane_center.x - m_plane_size.x / 2.0f);
 
-    float mappedY = (static_cast<float>(mousePixel.y) / m_pixel_size.y) * m_plane_size.y
-                    + (m_plane_center.y - m_plane_size.y / 2.0f);
+	float mappedY = (static_cast<float>(mousePixel.y) / m_pixel_size.y) * m_plane_size.y
+		+ (m_plane_center.y - m_plane_size.y / 2.0f);
 
-    return Vector2f(mappedX, mappedY);
+	return Vector2f(mappedX, mappedY);
+
 }
+
 
 void ComplexPlane::updateRender() {
 	if (m_state == State::CALCULATING) {
